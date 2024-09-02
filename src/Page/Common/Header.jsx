b37ec3220ee1350367ebd20e4f79eb90cc2befa5
jmjubaer <jmjubaer3927@gmail.com>
2024-09-02 23:32:17 +0600

    complete header components

--- src/Page/Common/Header.jsx
+++ src/Page/Common/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Header.css";
 import logo from "../../assets/Logo.png";
+import { Link } from "react-router-dom";
 const Header = () => {
     return (
         <div className='bg-white'>
             <div className='header_container'>
-                <img src={logo} />
+                <Link to={"/"}>
+                    <img src={logo} />
+                </Link>
             </div>
         </div>
     );
